2b915c7db0eb267ff1097c5b400b2c193b302212
corsair <xiangp126@126.com>
2018-02-27 03:11:35 -0500

    fix bug: heapsort

--- src/heapsort.cpp
+++ src/heapsort.cpp
@@ -9,8 +9,6 @@ void heapSort(vector<int> &nums) {
     int rear = N - 1;
     // take use of 'max peak heap'
     makeHeap(nums);
-    printArray(nums);
-    return;
     for (int i = rear; i >= 1; --i) {
         swap(nums[0], nums[i]);
         percolateDown(nums, 0, i - 1);
@@ -21,10 +19,9 @@ void makeHeap(vector<int> &nums) {
     // index 0 -> N - 1
     int N = nums.size();
     int rear = N - 1;
-    // for (int i = rear / 2; i >= 0; --i) {
-        // percolateDown(nums, i, rear);
-    // }
-    percolateDown(nums, (rear - 1) / 2, rear);
+    for (int i = (rear - 1) / 2; i >= 0; --i) {
+        percolateDown(nums, i, rear);
+    }
 }
 
 // format max peak heap
@@ -32,25 +29,20 @@ void percolateDown(vector<int> &nums, int front, int rear) {
     if (front > rear) {
         return;
     }
-    int pIndex   = front;
-    int sentinel = nums[pIndex];
-    int childLIndex = pIndex * 2 + 1;
-    int childRIndex = childLIndex + 1;
-    int childIndex  = childLIndex;
-    while (pIndex < rear && childLIndex <= rear) {
-        childLIndex = pIndex * 2 + 1;
-        childRIndex = childLIndex + 1;
-        if (childRIndex <= rear && nums[childRIndex] > nums[childLIndex]) {
-            childIndex = childRIndex;
-        } else {
-            childIndex = childLIndex;
+    int pIndex = front;
+    int childIndex = 0, childRIndex = 0;
+    // bug point: pIndex * 2 + 1 must effect here
+    while (pIndex < rear && pIndex * 2 + 1 <= rear) {
+        childIndex  = pIndex * 2 + 1;
+        childRIndex = childIndex + 1;
+        if (childRIndex <= rear && nums[childIndex] < nums[childRIndex]) {
+            ++childIndex;
         }
-        if (sentinel < nums[childIndex]) {
-            nums[pIndex] = nums[childIndex];
+        if (nums[pIndex] < nums[childIndex]) {
+            swap(nums[pIndex], nums[childIndex]);
             pIndex = childIndex;
         } else {
             break;
         }
     }
-    nums[pIndex] = sentinel;
 }
