f53daafb96aa2b4e366f097045178b5b2a8db538
Peng Xiang <hi.pxiang@gmail.com>
2018-12-25 18:33:19 +0800

    update demo for kernel list

--- kernelList/list_demo.c
+++ kernelList/list_demo.c
@@ -1,51 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "list.h"
 
 const int N = 3;
-struct my_obj {
-    int to;
-    int from;
+struct my_pair {
+    int key;
+    int value;
     list_head_t list;
 };
 
-void dump_entry(struct my_obj *obj) {
-    printf("from %d to %d.\n", obj->from, obj->to);
+void dump_entry(struct my_pair *obj) {
+    printf("key %d value %d.\n", obj->key, obj->value);
 }
 
 int main(int argc, char *argv[])
 {
     int i;
+    struct my_pair *myPair;
+
+    srand(time(NULL));
+
+    /* define and init head */
     list_head_t head;
     INIT_LIST_HEAD(&head);
 
-    struct my_obj *myObj = (struct my_obj *)malloc(sizeof(struct my_obj) * N);
-    free((void *)myObj);
     /* init entry itself */
-    for (int i = 0; i < N; ++i) {
-        myObj[i].from = i;
-        myObj[i].to = myObj[i].from + 1;
-    }
-
-    /* add each entry into list */
-    for (int i = 0; i < N; ++i) {
-        list_add_tail(&myObj[i].list, &head);
+    for (i = 0; i < N; ++i) {
+        myPair = (struct my_pair *) malloc(sizeof(struct my_pair));
+        myPair->key = i;
+        myPair->value = rand();
+        /* add each entry into list */
+        list_add_tail(&myPair->list, &head);
     }
 
     printf("Demo for list_for_each_entry\n");
-    struct my_obj *pos;
+    struct my_pair *pos;
     list_for_each_entry(pos, &head, list) {
         dump_entry(pos);
     }
 
+    struct my_pair *n;
+    int target = 1;
     printf("\nDel second element\n");
-    i = 1;
-    list_del(&myObj[i].list);
+    list_for_each_entry_safe(pos, n, &head, list) {
+        if (pos->key == target) {
+            list_del(&pos->list);
+            free(pos);
+        }
+    }
+
+    printf("\nAfter Del\n");
     list_for_each_entry(pos, &head, list) {
         dump_entry(pos);
     }
-
-    dump_entry(&myObj[i]);
 
     return 0;
 }
